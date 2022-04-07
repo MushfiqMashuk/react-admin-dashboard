c8e417e23767da1dacbba00e4f0c0b29e0f97e8f
Mushfiq Mashuk <alberteinstein3626@gmail.com>
2022-04-08 03:59:41 +0600

    Routing Setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 function App() {
-  return <div >Hello World</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

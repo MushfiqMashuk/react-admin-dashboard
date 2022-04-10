fd3f3f9340cfa83c5cdbc42111e43f999af24f81
Mushfiq Mashuk <alberteinstein3626@gmail.com>
2022-04-10 23:56:14 +0600

    All Routing Setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import List from "./pages/List";
 import Login from "./pages/Login";
+import NewPage from "./pages/NewPage";
+import SinglePage from "./pages/SinglePage";
 
 function App() {
   return (
@@ -8,7 +11,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="login" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/newPage" element={<NewPage />} />
+          <Route path="/singlePage" element={<SinglePage />} />
+          <Route path="/list" element={<List />} />
         </Routes>
       </BrowserRouter>
     </div>
